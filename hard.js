3b8e92ef47b6f57b326070876be111b95097915f
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-02 14:55:48 -0500

    hard.js complete

--- hard.js
+++ hard.js
@@ -7,9 +7,15 @@ var c = b + a;
 
 // Q: What is the value of c?
 
+// 23
+
 
 // Write to the best if your understanding
 // why c is the value that it is.
+
+// c is 23 because a is a string. When b is added to a, the 
+// number 2 is simply added as part of the string.
+
 
 
 
@@ -18,10 +24,17 @@ var str = "Version " + 5 + 1;
 
 // Q: What is the value of str?
 
+// Version 51
+
 
 // Write to the best if your understanding
 // why str is the value that it is. How could
 // it be altered to result in 6 instead of 51?
+
+// str is Version 51 because Version is a string. When 5 and 1 are 
+// added to version, they become part of the string and are not added together.
+
+// str = "Version" + (5 + 1);
 
 
 
