a1f625826373c6d891dec45985c5cf3557860ad0
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-02 17:08:08 -0500

    comlpleted nightmare mode

--- nightmare.js
+++ nightmare.js
@@ -32,27 +32,26 @@ for (var i = 1; i <= 100; i++) {
 
 // This part works!!!! Holy crap :-)
 
+
 for (var i = 1; i <= 100; i++) {
   console.log(i);
 
   if (i % 3 == 0) {
     console.log("Fizz");
   }
+  else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+  else if (i % 3 == i % 5 ) { 
+    console.log("FizzBuzz");
+  } 
   else { 
     console.log(i);
-  } 
+  }
 }
 
 
 
-
-if ( i++ % 3 = 0) {
-    console.log("Fizz");
-} else if (i++ % 3 = 0; i++ % 5 = 0) { 
-    print = 'FizzBuzz'
-} else { 
-    console.log(i);
-} 
 
 
 
